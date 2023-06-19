609df799a33c60dfa234c6a8909aa261fa55af79
mac <nutritionmegastorepalmira@gmail.com>
2023-06-18 23:11:21 -0500

    configuraciones de ios terminada

--- lib/services/push_notifications_service.dart
+++ lib/services/push_notifications_service.dart
@@ -5,6 +5,9 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
+// Key ID:NV4TU24ARL
+
+
 class PushNotificationService {
   static final FirebaseMessaging message = FirebaseMessaging.instance;
   static final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
@@ -12,7 +15,7 @@ class PushNotificationService {
   static final StreamController<String> _messageStream =
       StreamController.broadcast();
 
-static Stream<String> get messageStream => _messageStream.stream;
+  static Stream<String> get messageStream => _messageStream.stream;
 
   static closeController() {
     _messageStream.close();
@@ -35,10 +38,31 @@ static Stream<String> get messageStream => _messageStream.stream;
 
   static Future initializeApp() async {
     await Firebase.initializeApp();
-    token = await message.getToken();
-    print(token);
-    FirebaseMessaging.onBackgroundMessage(_backgroundHandler);
-    FirebaseMessaging.onMessage.listen(_onMessageHandler);
-    FirebaseMessaging.onMessageOpenedApp.listen(_onMessageOpenApp);
+    await requestPermission();
+  }
+
+  static requestPermission() async {
+    NotificationSettings settings = await message.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+    print('User push notification status: ${settings.authorizationStatus}');
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      token = await message.getToken();
+      print(token);
+      FirebaseMessaging.onBackgroundMessage(_backgroundHandler);
+      FirebaseMessaging.onMessage.listen(_onMessageHandler);
+      FirebaseMessaging.onMessageOpenedApp.listen(_onMessageOpenApp);
+    }
+
+    message.onTokenRefresh.listen((token) {
+      print('Token: $token');
+    });
   }
 }
